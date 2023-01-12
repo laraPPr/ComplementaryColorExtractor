56034c73e5e79859bd90c4b21b6b65b398549786
laraPPr <49882639+laraPPr@users.noreply.github.com>
2023-01-12 21:27:32 +0100

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,13 @@ app._static_folder = os.path.abspath("templates/static/")
 
 @app.route("/", methods=["GET"])
 def index():
-  title = "Complementary color Application,
+  title = "Complementary color Application"
   return render_template("layouts/palette_extractor.html", title=title)
 
+@app.route("/palette_extractor/")
+def paletteExtractor():
+  title = "Complementary color Application,
+  return render_template("layouts/palette_extractor.html", title=title)
+  
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=5000
