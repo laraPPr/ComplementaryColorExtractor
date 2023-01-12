a6082405acbcce4a8b70ce2ec6686520e4e390b5
laraPPr <49882639+laraPPr@users.noreply.github.com>
2023-01-12 21:24:42 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, jsonify, make_response, render_template, request
+
+from app = Flask(__name__)
+app.debug = False
+app._static_folder = os.path.abspath("templates/static/")
+
+@app.route("/", methods=["GET"])
+def index():
+  title = "Complementary color Application,
+  return render_template("layouts/palette_extractor.html", title=title)
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=5000
